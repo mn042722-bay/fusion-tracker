94ea7f9fc182f523a18d7bd4064bc7f437da2aeb
masa <your@email.com>
2026-02-17 23:15:21 +0900

    Add production CORS origin

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://fusion-tracker-frontend.onrender.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
